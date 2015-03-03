555a920330e615793b238d72255274ca7c558467
Robin Nicollet <robin.nicollet@gmail.com>
2015-03-03 13:52:49 +0100

    make static file handling work for most file types, + add example html page

--- src/core/RouterHandlerFactory.cpp
+++ src/core/RouterHandlerFactory.cpp
@@ -28,7 +28,7 @@ void RouterHandlerFactory::onServerStop() noexcept
 
 proxygen::RequestHandler* RouterHandlerFactory::onRequest(proxygen::RequestHandler* previousHandler, proxygen::HTTPMessage* message) noexcept
 {
-  std::regex staticFilesMatcher("/test\\.jpg", std::regex_constants::ECMAScript | std::regex_constants::icase);
+  std::regex staticFilesMatcher(".*\\.(ico|jpg|png|gif|js|html|css)", std::regex_constants::ECMAScript | std::regex_constants::icase);
   std::cout << "URL requested " << message->getURL() << std::endl;
   std::cout << std::regex_match(message->getURL(), staticFilesMatcher) << std::endl;
   if (std::regex_match(message->getURL(), staticFilesMatcher))
--- src/core/StaticFileHandler.cpp
+++ src/core/StaticFileHandler.cpp
@@ -1,6 +1,7 @@
 #include "core/StaticFileHandler.hpp"
 #include <sstream>
 #include <fstream>
+#include <regex>
 
 StaticFileHandler::StaticFileHandler()
 {
@@ -14,11 +15,37 @@ StaticFileHandler::~StaticFileHandler()
 
 void StaticFileHandler::onRequest(std::unique_ptr<proxygen::HTTPMessage> headers) noexcept
 {
-  std::cout << "Static file request for " << headers->getURL() << std::endl;
+  std::string requestedUrl = headers->getURL();
+  std::cout << "Static file request for " << requestedUrl << std::endl;
   p_responseBuilder = new proxygen::ResponseBuilder(downstream_);
-  std::string filename = "public/image.jpg";
+  std::string filename = "public" + requestedUrl;
 
-  std::ifstream file(filename.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+  std::string contentType;
+  std::regex imageFilesMatcher(".*\\.(ico|jpg|png|gif)", std::regex_constants::ECMAScript | std::regex_constants::icase);
+  std::regex textFilesMatcher(".*\\.(html|css|js)", std::regex_constants::ECMAScript | std::regex_constants::icase);
+  std::smatch matches;
+  bool openBinary = false;
+  if (std::regex_match(requestedUrl, matches, imageFilesMatcher))
+  {
+    contentType = "image/" + matches[1].str();
+    openBinary = true;
+  }
+  else if (std::regex_match(requestedUrl, matches, textFilesMatcher))
+  {
+    std::string append = matches[1].str();
+    if (append == "js")
+    {
+      append = "javascript";
+    }
+    contentType = "text/" + append + "; charset=utf-8";
+  }
+
+  std::ios_base::openmode mode = std::ios::in | std::ios::ate;
+  if (openBinary) {
+    mode = std::ios::in | std::ios::binary | std::ios::ate;
+  }
+
+  std::ifstream file(filename.c_str(), mode);
   if (file.is_open())
   {
     int fileSize = file.tellg();
@@ -30,7 +57,7 @@ void StaticFileHandler::onRequest(std::unique_ptr<proxygen::HTTPMessage> headers
     file.close();
 
     p_responseBuilder->status(200, "OK")
-      .header("Content-Type", "image/jpg")
+      .header("Content-Type", contentType)
       .body(std::move(fileContent))
       .sendWithEOM();
   }
